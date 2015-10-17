ed4c79c76b60f4b2a4a4cd7f4f0c2cba53aaa8fb
EPICmynamesBG <mynamesbg@gmail.com>
2015-10-17 18:40:07 -0400

    Blend test

--- combine_images.py
+++ combine_images.py
@@ -2,15 +2,15 @@ from PIL import Image
 
 def combineImages():
     initialImage = Image.open("image.jpg")
-    #inImageSize = initialImage.size()
     stickerImage = Image.open("images/creepy.png")
+    stickerSize = stickerImage.size()
 
-    #for (int i = 0; i < inImageSize[0]; i++){
-    #
-    #}
+    a = initialImage.load() #loads pixel data
+    b = stickerImage.load()
 
-    result = Image.blend(initialImage, stickerImage, 0.5)
+    for (int w = 0; w < stickerSize[0]; w++) :#width
+        for (int h = 0; h < stickerSize[1]; h++) : #height
+            a[w, h] = (a[w,h] * 0.5() + (b[w,h] * 0.5)
 
-    result.save('Blended.png')
 
-
+    initialImage.save('Blended.png')
