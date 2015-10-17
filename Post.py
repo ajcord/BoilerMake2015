b29821e922240e83163bd123225816a0a0b6d682
Brandon Groff <mynamesbg@gmail.com>
2015-10-17 03:24:32 -0400

    Adding image folders

--- Post.py
+++ Post.py
@@ -1,20 +1,18 @@
 from twython import Twython
 import json
 
-APP_KEY = '4Rye2VvrGTaPDlSmxwxgwU1gQ'
-APP_SECRET = 'ddCbtpCs2ePsRXcriSGwJGrmrzZKhwSYVFq1MdxML3iKwsJfnV'
+def push_post(imageFilepath, post):
+	APP_KEY = '4Rye2VvrGTaPDlSmxwxgwU1gQ'
+	APP_SECRET = 'ddCbtpCs2ePsRXcriSGwJGrmrzZKhwSYVFq1MdxML3iKwsJfnV'
 
-OAUTH_TOKEN = '3911053409-OxVKx19eZcLz3fkhUeJyli8En2tNKi2nCQzNoQW'
-OAUTH_TOKEN_SECRET = 'OOuk4Czrbv4E1dNpUWF2uiQwj3lzXdcUHl9Q3DPHa1RhR'
+	OAUTH_TOKEN = '3911053409-OxVKx19eZcLz3fkhUeJyli8En2tNKi2nCQzNoQW'
+	OAUTH_TOKEN_SECRET = 'OOuk4Czrbv4E1dNpUWF2uiQwj3lzXdcUHl9Q3DPHa1RhR'
 
-twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-
-imageFilepath = 'Twitter_logo.png'
-photo = open(imageFilepath,  'rb')
-if (photo is None) :
-    print "Photo is None"
-else :
-    response = twitter.upload_media(media=photo)
-    twitter.update_status(status="Just testing out the Twitter API", media_ids=[response['media_id']])
-
-    print "Finished"
+	twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+	photo = open(imageFilepath,  'rb')
+	if (photo is None) :
+		return False
+	else :
+		response = twitter.upload_media(media=photo)
+		twitter.update_status(status=post, media_ids=[response['media_id']])
+		return True
