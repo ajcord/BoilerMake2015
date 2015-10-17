fa7ad4551e14a46b05d6e8cbffb8deecd919a5c2
Alex Cordonnier <alexjcord@gmail.com>
2015-10-17 07:48:48 -0400

    Namespace

--- capture.py
+++ capture.py
@@ -26,7 +26,7 @@ def capture_loop():
             print("Smile!")
             camera.capture("image.jpg")
             # upload_to_server("image.jpg")
-            push_post("image.jpg", "Test")
+            post.push_post("image.jpg", "Test")
 
         pad0alreadyPressed = pad0pressed
         
