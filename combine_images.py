fab76fa2f02f81a66fd0e2868ee383eb8658985d
EPICmynamesBG <mynamesbg@gmail.com>
2015-10-17 21:04:46 -0400

    More stickers!

--- combine_images.py
+++ combine_images.py
@@ -4,7 +4,7 @@ import random
 def addSticker():
     initialImage = Image.open("image.jpg")
     if (random.randint(0, 2) == 0):
-        imageArray = ["creepy", "ghost", "skeleton", "web"]
+        imageArray = ["creepy", "ghost", "skeleton", "web", "Jack-o-Lantern","trump"]
         index = random.randint(0, len(imageArray) - 1)
         print(index)
         stickerImage = Image.open("images/"+imageArray[index]+".png")
