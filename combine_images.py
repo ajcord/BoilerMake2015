415a911c68d775e161cffcc9af77ce4ba07ca6d9
EPICmynamesBG <mynamesbg@gmail.com>
2015-10-17 20:15:02 -0400

    Randomized sticker addition and sticker image

--- combine_images.py
+++ combine_images.py
@@ -1,9 +1,15 @@
 from PIL import Image
+import random
 
 def addSticker():
     initialImage = Image.open("image.jpg")
-    stickerImage = Image.open("images/creepy.png")
+    if (random.randint(0, 2) == 1):
+        imageArray = ["creepy", "ghost", "skeleton"]
+        imageString = random.randint(0, len(imageArray) - 1)
+        stickerImage = Image.open("images/"+imageString+".png")
 
-    initialImage.paste(stickerImage, (0,0), stickerImage)
+        initialImage.paste(stickerImage, (random.randint(0, 520),random.randInt(0, 280)), stickerImage)
+    else :
+        #don't add sticker
 
     initialImage.save("blended.jpg", quality=95)
