dfec48e54c895192916f6b76ec49d72e674bf1b2
EPICmynamesBG <mynamesbg@gmail.com>
2015-10-17 20:35:28 -0400

    fixed array over index

--- combine_images.py
+++ combine_images.py
@@ -5,7 +5,7 @@ def addSticker():
     initialImage = Image.open("image.jpg")
     if (random.randint(0, 2) == 0):
         imageArray = ["creepy", "ghost", "skeleton", "web"]
-        index = random.randint(0, len(imageArray))
+        index = random.randint(0, len(imageArray) - 1)
         stickerImage = Image.open("images/"+imageArray[index]+".png")
 
         initialImage.paste(stickerImage, (random.randint(0, 520),random.randint(0, 280)), stickerImage)
