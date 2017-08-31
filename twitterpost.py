5b9f40124ea6e154d48f93b4d3e3b3a9748897a6
Alex Cordonnier <alexjcord@gmail.com>
2017-08-30 21:47:36 -0700

    Removed API keys; the old ones in the commit history have been invalidated so you can't use them 😉

--- twitterpost.py
+++ twitterpost.py
@@ -2,11 +2,11 @@ from twython import Twython
 import json
 
 def push_post(imageFilepath, post):
-	APP_KEY = '4Rye2VvrGTaPDlSmxwxgwU1gQ'
-	APP_SECRET = 'ddCbtpCs2ePsRXcriSGwJGrmrzZKhwSYVFq1MdxML3iKwsJfnV'
+	APP_KEY = 'your key here'
+	APP_SECRET = 'your secret here'
 
-	OAUTH_TOKEN = '3911053409-OxVKx19eZcLz3fkhUeJyli8En2tNKi2nCQzNoQW'
-	OAUTH_TOKEN_SECRET = 'OOuk4Czrbv4E1dNpUWF2uiQwj3lzXdcUHl9Q3DPHa1RhR'
+	OAUTH_TOKEN = 'your oauth token here'
+	OAUTH_TOKEN_SECRET = 'your oauth secret here'
 
 	twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 	photo = open(imageFilepath,  'rb')
